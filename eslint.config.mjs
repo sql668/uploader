7fa37d1d17824b83b9cb83ec16335696b9d2cae8
sql123 <mike@magic.org>
2025-10-19 19:45:27 +0800

    ci: 环境搭建

---
+++ eslint.config.mjs
@@ -0,0 +1,33 @@
+// import js from '@eslint/js';
+// import globals from 'globals';
+// import tseslint from 'typescript-eslint';
+// import { defineConfig } from 'eslint/config';
+// import eslintPluginPrettier from 'eslint-plugin-prettier';
+
+// export default defineConfig([
+//   {
+//     files: ['**/*.{js,mjs,cjs,ts,mts,cts}'],
+//     plugins: { js },
+//     extends: ['js/recommended'],
+//     languageOptions: { globals: globals.browser },
+//   },
+//   tseslint.configs.recommended,
+//   eslintPluginPrettier,
+// ]);
+
+// @ts-check
+
+import { tanstackConfig } from '@tanstack/config/eslint'
+
+export default [
+  ...tanstackConfig,
+  {
+    name: 'tanstack/temp',
+    rules: {
+      '@typescript-eslint/naming-convention': 'off',
+      '@typescript-eslint/no-unnecessary-condition': 'off',
+      '@typescript-eslint/no-unsafe-function-type': 'off',
+      'no-self-assign': 'off',
+    },
+  },
+]
